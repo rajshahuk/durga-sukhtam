a38e8b44b6c760043dbe13f878504a00a8ebbfea
Rajesh Shah <raj@12nines.com>
2016-10-20 14:47:03 +0100

    Initial Commit

---
+++ Durga Sukhtam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Durga Sukhtam
+//
+//  Created by Rajesh Shah on 20/10/2016.
+//  Copyright © 2016 Rajesh Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
